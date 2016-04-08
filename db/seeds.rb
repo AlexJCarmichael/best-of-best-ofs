8f45f3cd74a6e88d1ba8fe2b933ae47a4e4217c2
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-08 14:58:04 -0500

    Seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create!(username: "George", password: "password")
+15.times do
+  list = List.create!(list_title: Faker::Hipster.sentence(4, false, 7), list_desc: Faker::Hipster.paragraph(2, false, 8), source_ref: "#", image_ref: "#", user_id: user.id)
+  10.times do
+    Vote.create!(user_id: user.id, list_id: list.id, up_vote: 1, down_vote: 0, aggregate_vote: 0 )
+  end
+end
