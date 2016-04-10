922664fc1529c2c7a2af467d962bd46327c99d1f
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-10 16:15:03 -0500

    Added rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,19 @@ Bundler.require(*Rails.groups)
 
 module Wolfpack
   class Application < Rails::Application
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin'   => '*',
-      'Access-Control-Allow-Methods'  => 'POST, PUT, DELETE, GET, OPTIONS',
-      'Access-Control-Request-Method' => '*',
-      'Access-Control-Allow-Headers'  => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    }
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.action_dispatch.default_headers = {
+    #   'Access-Control-Allow-Origin'   => '*',
+    #   'Access-Control-Allow-Methods'  => 'POST, PUT, DELETE, GET, OPTIONS',
+    #   'Access-Control-Request-Method' => '*',
+    #   'Access-Control-Allow-Headers'  => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    # }
+    # config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
