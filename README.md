8aaf86f2e2d95d5ee39143aab232bc50b5cfa761
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-10 17:58:50 -0500

    Added real data to the database.

--- README.md
+++ README.md
@@ -1,28 +1,81 @@
-== README
+#Using the API for the Best of's
+---
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+##Get requests
+#####Routes
+/api/lists : Acess the top 5 lists by aggreagate vote
+/api/list?page=2 : Acess next 5 lists by aggregate vote
+/api/post/:id : Access the show page for the list at that ID number.
 
-Things you may want to cover:
+Lists can be replaced with votes, users, and items. Items, users, or votes show all, not the top 5.
 
-* Ruby version
+###Sample
+Api call : http://localhost:3000/api/lists/1
 
-* System dependencies
+```{
+  "id": 1,
+  "image_ref": "#",
+  "source_ref": "http://www.imdb.com/chart/top",
+  "list_desc": "A user curated list of the top two-hundred and fifty movies in the databse",
+  "list_title": "The Internet Movie Database top movies.",
+  "user_id": 1,
+  "created_at": "2016-04-10T20:22:43.102Z",
+  "updated_at": "2016-04-10T20:22:43.102Z",
+  "list_type": "Movies",
+  "items": [
+    {
+      "id": 1,
+      "one": "The Shawshank Redemption",
+      "two": "The Godfather",
+      "three": "The Godfather: Part II",
+      "four": "The Dark Knight",
+      "five": "Schindler's List",
+      "six": "12 Angry Men",
+      "seven": "Pulp Fiction",
+      "eight": "The Lord of the Rings: The Return of the King",
+      "nine": "The Good, the Bad and the Ugly",
+      "ten": "Fight Club",
+      "list_id": 1,
+      "created_at": "2016-04-10T20:22:43.123Z",
+      "updated_at": "2016-04-10T20:22:43.123Z"
+    }
+  ]
+}
+```
+##Put requests
+Api call : put api/posts/:id
+Api call : put api/items/:id
+Api call : put api/users/:id
+Api call : put api/votes/:id
 
-* Configuration
+####Sample of params needed
+#####Users
 
-* Database creation
+username : The desired username change
+password : The desired password change
 
-* Database initialization
+#####Votes
 
-* How to run the test suite
+user_id : The user id associated eith the vote
+list_id : The user id associated eith the vote
+up_vote : If the vote was positive this should be 1, else 0
+down_vote : If the vote was negative this should be 1, else 0
 
-* Services (job queues, cache servers, search engines, etc.)
+#####Items
 
-* Deployment instructions
+Each item corresponds to an item on a top ten list.
+item[one]
+item[two]
+item[three]
+item[four]
+item[five]
+item[six]
+item[seven]
+item[eight]
+item[nine]
+item[ten]
 
-* ...
+#####Lists
 
+list_title : The title of the list being submitted.
 
-Please feel free to use a different markup language if you do not plan to run
-<tt>rake doc:app</tt>.
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user = User.create!(username: "George", password: "password")
-  list_one = List.create!(image_ref: "#", source_ref: "http://www.imdb.com/chart/top",
+  list_one = List.create!(image_ref: "http://www.tamswitmark.com/wp-content/uploads/2013/09/Wizard-of-Oz-RSC-and-MUNI1-541x346.jpg", source_ref: "http://www.imdb.com/chart/top",
     list_desc: "A user curated list of the top two-hundred and fifty movies in the databse",
     list_title: "The Internet Movie Database top movies.", list_type: "Movies", user_id: user.id)
   Item.create!(list_id: list_one.id, one: "The Shawshank Redemption", two: "The Godfather", three: "The Godfather: Part II",
@@ -17,7 +17,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_one.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_two = List.create!(image_ref: "#", source_ref: "http://www.billboard.com/charts/hot-100/1985-12-14",
+  list_two = List.create!(image_ref: "https://i.ytimg.com/vi/nKhN1t_7PEY/maxresdefault.jpg", source_ref: "http://www.billboard.com/charts/hot-100/1985-12-14",
     list_desc: "A list of the top selling albums and air play of the given time period. Week of 12/14/1985",
     list_title: "The Billboard Top 100 for December 14, 1985.", list_type: "Music", user_id: user.id)
   Item.create!(list_id: list_two.id, one: "Broken Wings", two: "Separate Lives (Theme From White Nights)", three: "Say You, Say Me (Title Song From White Nights)",
@@ -27,7 +27,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_two.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_three = List.create!(image_ref: "#", source_ref: "http://www.animenewsnetwork.com/encyclopedia/ratings-anime.php?top50=best_bayesian",
+  list_three = List.create!(image_ref: "http://www.animenewsnetwork.com/thumbnails/crop900x350/video/category/489/steinsgate_banner_1.jpg", source_ref: "http://www.animenewsnetwork.com/encyclopedia/ratings-anime.php?top50=best_bayesian",
     list_desc: "A list of the best rated, by users, anime. This is a bayesian estime which weights votes for those with less total votes overall.",
     list_title: "Top 50 Best Rated Anime (bayesian estime) from Anime News Network.", list_type: "Animation", user_id: user.id)
   Item.create!(list_id: list_three.id, one: "Steins;Gate (TV)", two: "	Fullmetal Alchemist: Brotherhood (TV)", three: "Clannad After Story (TV)",
@@ -37,7 +37,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_three.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_four = List.create!(image_ref: "#", source_ref: "http://www.complex.com/pop-culture/2013/01/the-25-comic-books-you-need-to-read-before-you-die/maus",
+  list_four = List.create!(image_ref: "http://images.complex.com/complex/image/upload/c_limit,fl_progressive,q_80,w_680/rtldtqdwcskjk2mvmgev.jpg", source_ref: "http://www.complex.com/pop-culture/2013/01/the-25-comic-books-you-need-to-read-before-you-die/maus",
     list_desc: "A list of the top 25 comics by Jason Serafino for Complex",
     list_title: "The 25 Comic Books You Need To Read Before You Die,", list_type: "Misc", user_id: user.id)
   Item.create!(list_id: list_four.id, one: "Maus (Raw, Pantheon Books)", two: "Watchmen (DC/Vertigo)", three: "Sandman (DC/Vertigo)",
@@ -48,7 +48,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_four.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_five = List.create!(image_ref: "#", source_ref: "http://www.rottentomatoes.com/top/bestofrt/",
+  list_five = List.create!(image_ref: "http://deadshirt.net/wp-content/uploads/2014/10/the-godfather-1.jpg", source_ref: "http://www.rottentomatoes.com/top/bestofrt/",
     list_desc: "Movies with 40 or more critic reviews vie for their place in history at Rotten Tomatoes. Eligible movies are ranked based on their Adjusted Scores.",
     list_title: "TOP 100 MOVIES OF ALL TIME", list_type: "Movies", user_id: user.id)
   Item.create!(list_id: list_five.id, one: "The Wizard of Oz (1939)", two: "The Third Man (1949)", three: "Citizen Kane (1941)",
@@ -58,7 +58,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_five.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_six = List.create!(image_ref: "#", source_ref: "http://thegreatestbooks.org/",
+  list_six = List.create!(image_ref: "http://ecx.images-amazon.com/images/I/41FLqHFwwuL._SX328_BO1,204,203,200_.jpg", source_ref: "http://thegreatestbooks.org/",
     list_desc: "A list of the greatest books of all time", list_title: "The Greatest Books", list_type: "Misc", user_id: user.id)
   Item.create!(list_id: list_six.id, one: "In Search of Lost Time by Marcel Proust", two: "Ulysses by James Joyce", three: "Don Quixote by Miguel de Cervantes",
   four: "Moby Dick by Herman Melville", five: "Hamlet by William Shakespeare", six: "War and Peace by Leo Tolstoy", seven: "The Odyssey by Homer",
@@ -67,7 +67,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_six.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_seven = List.create!(image_ref: "#", source_ref: "http://pitchfork.com/features/lists-and-guides/9763-the-best-music-videos-of-2015/",
+  list_seven = List.create!(image_ref: "http://cdn.pitchfork.com/longform/276/YIM_MusicVideos.png", source_ref: "http://pitchfork.com/features/lists-and-guides/9763-the-best-music-videos-of-2015/",
     list_desc: "From Nicki and Beyoncé’s ecstatic Coachella road trip, to David Bowie’s haunting interstellar hallucination, to Oneohtrix Point Never’s epic LARP-ing battle,
     to Charli XCX’s psychotic death-by-selfie freak show, to Kendrick’s powerful levitating act, here are our 20 favorite videos of the year.",
     list_title: "The Best Music Videos of 2015", list_type: "Music", user_id: user.id)
@@ -78,7 +78,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_seven.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_eight = List.create!(image_ref: "#", source_ref: "http://www.buzzfeed.com/annamenta/the-funniest-spongebob-squarepants-quotes#.gqPvoA3Wpz",
+  list_eight = List.create!(image_ref: "https://img.buzzfeed.com/buzzfeed-static/static/2015-09/23/17/enhanced/webdr04/enhanced-buzz-4672-1443043495-15.jpg?no-auto", source_ref: "http://www.buzzfeed.com/annamenta/the-funniest-spongebob-squarepants-quotes#.gqPvoA3Wpz",
     list_desc: "We asked the BuzzFeed Community to share their favorite quotes from SpongeBob SquarePants. Here are the hilarious responses",
     list_title: "25 Of The Most Hilarious “SpongeBob” Quotes", list_type: "Misc", user_id: user.id)
   Item.create!(list_id: list_eight.id, one: "When Patrick asked the question on every musician’s mind", two: "When SpongeBob coined a mantra of body positivity.",
@@ -91,7 +91,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_eight.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_nine = List.create!(image_ref: "#", source_ref: "http://www.computerworld.com/article/2473298/application-development/120001-GitHub-s-top-10-rock-star-projects.html",
+  list_nine = List.create!(image_ref: "https://assets-cdn.github.com/images/modules/logos_page/Octocat.png", source_ref: "http://www.computerworld.com/article/2473298/application-development/120001-GitHub-s-top-10-rock-star-projects.html",
     list_desc: "Creators and contributors shed light on GitHub's most-popular projects -- and sing praise to the code-sharing site's impact on their community and code",
     list_title: "GitHub's top 10 most popular projects", list_type: "Misc", user_id: user.id)
   Item.create!(list_id: list_nine.id, one: "Bootstrap", two: "Node.js", three: "JQuery", four: "HTML5 Boilerplate", five: "Ruby on Rails",
@@ -100,7 +100,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_nine.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_ten = List.create!(image_ref: "#", source_ref: "http://listverse.com/2013/04/14/10-ridiculously-tiny-animals/",
+  list_ten = List.create!(image_ref: "http://i1.wp.com/listverse.com/wp-content/uploads/2013/04/20100903-einstein-3-600x411.jpg?resize=600%2C411", source_ref: "http://listverse.com/2013/04/14/10-ridiculously-tiny-animals/",
     list_desc: "Every respected scientist will be able to tell you that there is a direct relationship with how small something is,
     and how adorable it is. Kittens, mini coopers, babies, hell even baby shoes are so adorable it will cause a person’s voice to jump several octaves up.
     But grizzly bears, hummers, fat guys etc. just don’t have the same effect, impressive as they are. So here are ten animals so ridiculously tiny,
@@ -113,7 +113,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_ten.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_eleven = List.create!(image_ref: "#", source_ref: "http://www.businessinsider.com/best-david-letterman-top-10-lists-2014-4",
+  list_eleven = List.create!(image_ref: "http://static2.businessinsider.com/image/533dda796bb3f7671b0c7568-960/letterman%20late%20show.jpeg", source_ref: "http://www.businessinsider.com/best-david-letterman-top-10-lists-2014-4",
     list_desc: "To compile the ranked list, we took into consideration top-viewed lists from YouTube and talked-about lists across the Internet. We also looked at prior anniversary lists from Letterman himself",
     list_title: "The Top 10 David Letterman Top 10 Lists", list_type: "Misc", user_id: user.id)
   Item.create!(list_id: list_eleven.id, one: "Top 10 George Bush Moments", two: "Top Ten List of Top 10 Favorite California Names",
@@ -125,7 +125,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_eleven.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_twelve = List.create!(image_ref: "#", source_ref: "http://blog.honeyfeed.fm/top-10-gundam-series-since-1979-best-recommendations/",
+  list_twelve = List.create!(image_ref: "http://blog.honeyfeed.fm/wp-content/uploads/2015/05/zeta-gundam-dvd-300x424.jpg", source_ref: "http://blog.honeyfeed.fm/top-10-gundam-series-since-1979-best-recommendations/",
     list_desc: "A blog post detailing the websites top ten seires in the Gundam franchise",list_title: "Top 10 Gundam Series Since 1979 [Best Recommendations]",
     list_type: "Animation", user_id: user.id)
   Item.create!(list_id: list_twelve.id, one: "Zeta Gundam", two: "Gundam 08th MS Team", three: "Mobile Suit Gundam", four: "Gundam Unicorn",
@@ -135,7 +135,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_twelve.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_thirteen = List.create!(image_ref: "#", source_ref: "http://www.buzzfeed.com/mjs538/the-40-most-important-corgis-of-2014#.diE1EgQ3pW",
+  list_thirteen = List.create!(image_ref: "https://img.buzzfeed.com/buzzfeed-static/static/2014-12/9/12/enhanced/webdr05/enhanced-1358-1418146830-31.jpg?no-auto", source_ref: "http://www.buzzfeed.com/mjs538/the-40-most-important-corgis-of-2014#.diE1EgQ3pW",
     list_desc: "The most important end-of-the-year list is back.", list_title: "The 40 Most Important Corgis Of 2014", list_type: "Animals", user_id: user.id)
   Item.create!(list_id: list_thirteen.id, one: "Corgnelius and Stumphrey", two: "The Corgi That Fell in the Mud", three: "The Corgi That Was Used for Scale",
   four: "Penny", five: "The Corgi That Came Out of a Package of Cookies", six: "The Corgi and Cat That Became Best Friends", seven: "Otis",
@@ -144,7 +144,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_thirteen.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_fourteen = List.create!(image_ref: "#", source_ref: "http://www.ranker.com/crowdranked-list/my-favorite-cartoons-of-all-time",
+  list_fourteen = List.create!(image_ref: "https://images2.alphacoders.com/445/445517.jpg", source_ref: "http://www.ranker.com/crowdranked-list/my-favorite-cartoons-of-all-time",
     list_desc: "A site that ranks items based on user votes.", list_title: "The Best Cartoons of All Time", list_type: "Animation", user_id: user.id)
   Item.create!(list_id: list_fourteen.id, one: "Looney Toons", two: "The Simpsons", three: "Tom and Jerry", four: "The Flinstones",
   five: "Scooby-Doo, Where are you!", six: "SpongeBob SquarePants", seven: "Batman: The Animated Series", eight: "Avatar: The Last Airbender",
@@ -153,7 +153,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_fourteen.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_fifteen = List.create!(image_ref: "#", source_ref: "http://www.thetoptens.com/best-jelly-belly-flavors/",
+  list_fifteen = List.create!(image_ref: "http://www.partyexcuses.com/images/uploads/holidayimages/bigstock-Small-Jelly-Beans-55958012.jpg", source_ref: "http://www.thetoptens.com/best-jelly-belly-flavors/",
     list_desc: "A site that accepts user votes and coments to rank items",
     list_title: "Best Jelly Belly Flavors", list_type: "Food", user_id: user.id)
   Item.create!(list_id: list_fifteen.id, one: "Green Apple", two: "Toasted Marshmallow", three: "Buttered Popcorn", four: "Tutti Frutti",
@@ -162,7 +162,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_fifteen.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_sixteen = List.create!(image_ref: "#", source_ref: "http://screenrant.com/10-best-action-movies-ever-list/",
+  list_sixteen = List.create!(image_ref: "http://screenrant.com/wp-content/uploads/Best-Action-Movies-Terminator-2.jpg", source_ref: "http://screenrant.com/10-best-action-movies-ever-list/",
     list_desc: "A list of the top ten action movies by Chris Agar of Screenrant. Ordered by release year.", list_title: "10 Best Action Movies Ever Made", list_type: "Movies", user_id: user.id)
   Item.create!(list_id: list_sixteen.id, two: "FIRST BLOOD (1982)", three: "LETHAL WEAPON (1987)", four: "DIE HARD (1988)", five: "TERMINATOR 2: JUDGMENT DAY (1991)",
   six: "POINT BREAK (1991)", seven: "BAD BOYS (1995)", eight: "THE MATRIX (1999)", nine: "THE BOURNE ULTIMATUM (2007)",
@@ -171,7 +171,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_sixteen.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_seventeen = List.create!(image_ref: "#", source_ref: "http://splitsider.com/2015/03/the-20-best-comedy-movies-of-all-time/",
+  list_seventeen = List.create!(image_ref: "https://awlnetwork-static-theawl.netdna-ssl.com//wp-content/uploads/sites/2/2015/03/20bestmovies-640x259.jpg", source_ref: "http://splitsider.com/2015/03/the-20-best-comedy-movies-of-all-time/",
     list_desc: "A list of the 20 best Comedies by John Wenzel of Splitsider",
     list_title: "The 20 Best Comedy Movies of All Time", list_type: "Movies", user_id: user.id)
   Item.create!(list_id: list_seventeen.id, one: "Blazing Saddles (1974)", two: "Dr. Strangelove (Or How I Learned to Stop Worrying and Love the Bomb) (1964)",
@@ -181,7 +181,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_seventeen.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_eighteen = List.create!(image_ref: "#", source_ref: "http://www.computerworld.com/article/2473298/application-development/120001-GitHub-s-top-10-rock-star-projects.html",
+  list_eighteen = List.create!(image_ref: "https://upload.wikimedia.org/wikipedia/en/9/97/Gurren-lagann-16-.jpg", source_ref: "http://blog.honeyfeed.fm/top-10-anime-made-by-gainax/",
     list_desc: "A blog post detailing the websites top ten anime series by the studio Gainax",
     list_title: "Top 10 Anime Made by Gainax [Best Recommendations]", list_type: "Anime", user_id: user.id)
   Item.create!(list_id: list_eighteen.id, one: "Neon Genesis Evangelion (Shin Seiki Evangelion)", two: "Tengen Toppa Gurren Lagann",
@@ -192,7 +192,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_eighteen.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_nineteen = List.create!(image_ref: "#", source_ref: "https://en.wikipedia.org/wiki/List_of_best-selling_game_consoles#All_game_consoles",
+  list_nineteen = List.create!(image_ref: "http://www.gadgetguysnc.com/wp-content/uploads/2016/01/consoles.jpg", source_ref: "https://en.wikipedia.org/wiki/List_of_best-selling_game_consoles#All_game_consoles",
     list_desc: "A list of Video game consoles ranked by units sold.", list_title: "All Game Consoles", list_type: "Video Games", user_id: user.id)
   Item.create!(list_id: list_nineteen.id, one: "Playstation 2", two: "Nintendo DS", three: "Game Boy/Game Boy Color", four: "Playstation", five: "Wii",
   six: "Xbox360", seven: "Playstation 3", eight: "Playstation Portable", nine: "Game Boy Advance", ten: "Nintendo Entertainment System")
@@ -200,7 +200,7 @@ user = User.create!(username: "George", password: "password")
     Vote.create!(list_id: list_nineteen.id, user_id: user.id, up_vote: 1, down_vote: 0)
   end
 
-  list_twenty = List.create!(image_ref: "#", source_ref: "https://www.vg247.com/2014/08/29/50-best-rpgs-ever/",
+  list_twenty = List.create!(image_ref: "http://assets.vg247.com/current//2014/08/skyrim.jpg", source_ref: "https://www.vg247.com/2014/08/29/50-best-rpgs-ever/",
     list_desc: "A list of the 50 best RPGs by VG24/7", list_title: "The 50 best RPGs EVER", list_type: "Video Games", user_id: user.id)
   Item.create!(list_id: list_twenty.id, one: "The Elder Scrolls V: Skyrim", two: "Mass Effect", three: "Baldur’s Gate", four: "World of Warcraft",
   five: "Fallout (Interplay)", six: "Diablo 1 & 2", seven: "Dark Souls", eight: "Final Fantasy", nine: "Zelda 2: The Adventure of Link", ten: "The Witcher")
