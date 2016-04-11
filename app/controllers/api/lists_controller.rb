2fc6b4799545e0cb0f6a9ec9da2c6fa97af0ef51
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-10 23:33:59 -0500

    PRY DON'T FAIL ME NOW

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,5 +1,6 @@
 class Api::ListsController < ApplicationController
   def index
+    binding.pry
     if params[:specific] == "least"
       @list = List.select("lists.*, SUM(votes.up_vote - votes.down_vote) as aggregate_votes, SUM(votes.up_vote) as up_votes, SUM(votes.down_vote) as down_votes")
                    .joins(:votes)
